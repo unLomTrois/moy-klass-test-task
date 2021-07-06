acc09cd746856b630d427a6f812310f3a3989fad
unlomtrois <vadim.kuz02@gmail.com>
2021-07-06 14:25:11 +0300

    add sequelize models from dumb

--- src/db.js
+++ src/db.js
@@ -3,6 +3,68 @@ const { Sequelize, Model, DataTypes } = seq;
 
 const db = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
+  define: {
+    //prevent sequelize from pluralizing table names
+    freezeTableName: true,
+    timestamps: false
+  }
 });
+
+export class LessonStudent extends Model { }
+LessonStudent.init({
+  lesson_id: DataTypes.INTEGER,
+  student_id: DataTypes.STRING,
+  visit: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false
+  }
+}, { sequelize: db, modelName: 'lesson_students' });
+
+export class LessonTeacher extends Model { }
+LessonTeacher.init({
+  lesson_id: DataTypes.INTEGER,
+  teacher_id: DataTypes.STRING,
+}, { sequelize: db, modelName: 'lesson_teachers' });
+
+
+export class Lesson extends Model { }
+Lesson.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true
+  },
+  date: {
+    type: DataTypes.DATE,
+    allowNull: false
+  },
+  title: DataTypes.CHAR(100),
+  status: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0
+  },
+}, { sequelize: db, modelName: 'lessons' });
+
+export class Student extends Model { }
+Student.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true
+  },
+  name: DataTypes.CHAR(10)
+}, { sequelize: db, modelName: 'students' });
+
+
+export class Teacher extends Model { }
+Teacher.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true
+
+  },
+  name: DataTypes.CHAR(10)
+}, { sequelize: db, modelName: 'teachers' });
 
 export { db }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import express from 'express'
-import { db } from './db.js'
+import { db, Lesson } from './db.js'
+
 const app = express()
 const port = 3000
 
-
 app.get('/', async (req, res) => {
-  const lesson = await db.query("select * from lessons")
+  const lessons = await Lesson.findAll()
 
-  console.log(lesson)
+  console.log(lessons)
 
-  res.send(lesson)
+  res.send(lessons)
 })
 
-app.listen(port, () => {
+app.listen(port, async () => {
+  await db.sync()
   console.log(`Example app listening at http://localhost:${port}`)
 })
