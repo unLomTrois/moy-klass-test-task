21e74eeb3f90bdebf2a50f059a0ce557e3cec0c2
unlomtrois <vadim.kuz02@gmail.com>
2021-07-06 21:18:39 +0300

    complete 1st task

--- src/db.js
+++ src/db.js
@@ -1,70 +1,115 @@
-import seq from 'sequelize';
+import seq from "sequelize";
 const { Sequelize, Model, DataTypes } = seq;
 
 const db = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres',
+  dialect: "postgres",
   define: {
     //prevent sequelize from pluralizing table names
     freezeTableName: true,
-    timestamps: false
-  }
+    timestamps: false,
+    underscored: true,
+  },
 });
 
-export class LessonStudent extends Model { }
-LessonStudent.init({
-  lesson_id: DataTypes.INTEGER,
-  student_id: DataTypes.STRING,
-  visit: {
-    type: DataTypes.BOOLEAN,
-    defaultValue: false
-  }
-}, { sequelize: db, modelName: 'lesson_students' });
-
-export class LessonTeacher extends Model { }
-LessonTeacher.init({
-  lesson_id: DataTypes.INTEGER,
-  teacher_id: DataTypes.STRING,
-}, { sequelize: db, modelName: 'lesson_teachers' });
-
-
-export class Lesson extends Model { }
-Lesson.init({
-  id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true
+export class LessonStudent extends Model {}
+LessonStudent.init(
+  {
+    lesson_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "lessons",
+        key: "id",
+      },
+    },
+    student_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "students",
+        key: "id",
+      },
+    },
+    visit: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
   },
-  date: {
-    type: DataTypes.DATE,
-    allowNull: false
+  { sequelize: db, modelName: "lesson_students" }
+);
+
+export class LessonTeacher extends Model {}
+LessonTeacher.init(
+  {
+    lesson_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      references: {
+        model: "lessons",
+        key: "id",
+      },
+    },
+    teacher_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "teachers",
+        key: "id",
+      },
+    },
   },
-  title: DataTypes.CHAR(100),
-  status: {
-    type: DataTypes.INTEGER,
-    defaultValue: 0
+  { sequelize: db, modelName: "lesson_teachers" }
+);
+
+export class Lesson extends Model {}
+Lesson.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+    },
+    date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    title: DataTypes.CHAR(100),
+    status: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
   },
-}, { sequelize: db, modelName: 'lessons' });
+  { sequelize: db, modelName: "lessons" }
+);
 
-export class Student extends Model { }
-Student.init({
-  id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true
+export class Teacher extends Model {}
+Teacher.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+    },
+    name: DataTypes.CHAR(10),
   },
-  name: DataTypes.CHAR(10)
-}, { sequelize: db, modelName: 'students' });
+  { sequelize: db, modelName: "teachers" }
+);
 
+Lesson.belongsToMany(Teacher, { through: LessonTeacher });
+Teacher.belongsToMany(Lesson, { through: LessonTeacher });
 
-export class Teacher extends Model { }
-Teacher.init({
-  id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true
-
+export class Student extends Model {}
+Student.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+    },
+    name: DataTypes.CHAR(10),
   },
-  name: DataTypes.CHAR(10)
-}, { sequelize: db, modelName: 'teachers' });
+  { sequelize: db, modelName: "students" }
+);
 
-export { db }
+
+Lesson.belongsToMany(Student, { through: LessonStudent });
+Student.belongsToMany(Lesson, { through: LessonStudent });
+
+export { db };
--- src/index.js
+++ src/index.js
@@ -1,18 +1,136 @@
-import express from 'express'
-import { db, Lesson } from './db.js'
+import express from "express";
+import {
+  db,
+  Lesson,
+  LessonStudent,
+  LessonTeacher,
+  Student,
+  Teacher,
+} from "./db.js";
+import seq from "sequelize";
+const { Op } = seq;
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.get('/', async (req, res) => {
-  const lessons = await Lesson.findAll()
+app.get("/", async (req, res) => {
+  const { date, status, teacher_ids, students_count } = req.query;
 
-  console.log(lessons)
+  const lessons_per_page = parseInt(req.query.lessons_per_page ?? 5);
+  const page = parseInt(req.query.page ?? 0) * lessons_per_page;
 
-  res.send(lessons)
-})
+  if (page < 0) {
+    res.send({
+      error: "negative page",
+    });
+    return;
+  }
+
+  const dates = date
+    ?.split(",")
+    .map((d) => new Date(d))
+    .filter((d) => d instanceof Date && !isNaN(d));
+
+  const teacher_ids_arr = teacher_ids?.split(",").map((id) => parseInt(id));
+
+  const lesson_teachers = await LessonTeacher.findAll({
+    where: {
+      teacher_id: {
+        [Op.in]: teacher_ids_arr ?? [],
+      },
+    },
+  });
+
+  const lesson_ids_with_teachers = lesson_teachers.map(
+    (lesson) => lesson.lesson_id
+  );
+
+  const students_count_arr = students_count
+    ?.split(",")
+    .map((num) => parseInt(num));
+
+  const lesson_ids_with_student_count = (
+    await LessonStudent.findAll({
+      attributes: ["lesson_id"],
+      group: ["lesson_id"],
+      having:
+        students_count == undefined
+          ? seq.literal("COUNT(student_id) = 999")
+          : students_count_arr?.length == 2
+          ? seq.literal(
+              `COUNT(student_id) between ${students_count_arr[0]} and ${students_count_arr[1]}`
+            )
+          : seq.literal(`COUNT(student_id) = ${students_count_arr[0]}`),
+    })
+  ).map((group) => group.lesson_id);
+
+  const united_ids_arr = lesson_ids_with_student_count.filter((value) =>
+    lesson_ids_with_teachers.includes(value)
+  );
+
+  const where = {
+    ...(status === undefined ? {} : { status }),
+    ...(date === undefined
+      ? {}
+      : {
+          date: {
+            [Op.or]: {
+              [Op.between]: dates,
+              [Op.eq]: dates[0],
+            },
+          },
+        }),
+    ...(teacher_ids === undefined
+      ? {}
+      : {
+          id: {
+            [Op.in]: united_ids_arr.length
+              ? united_ids_arr
+              : lesson_ids_with_teachers,
+          },
+        }),
+    ...(students_count === undefined
+      ? {}
+      : {
+          id: {
+            [Op.in]: united_ids_arr.length
+              ? united_ids_arr
+              : lesson_ids_with_student_count,
+          },
+        }),
+  };
+
+  const lessons = await Lesson.findAll({
+    where,
+    limit: lessons_per_page,
+    offset: page,
+    include: [
+      {
+        model: Teacher,
+        through: {
+          attributes: [],
+        },
+      },
+      {
+        model: Student,
+        through: {
+          as: "lesson",
+          attributes: ["visit"],
+        },
+      },
+    ],
+    order: [["date"]],
+  });
+
+  res.send(lessons.map(lesson => {
+    const visit_count = lesson.students.map(student => student.lesson.visit).reduce((acc, value) => acc += value, 0)
+    lesson.setDataValue("visitCount", visit_count)
+
+    return lesson;
+  }));
+});
 
 app.listen(port, async () => {
-  await db.sync()
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  // await db.sync();
+  console.log(`Example app listening at http://localhost:${port}`);
+});
